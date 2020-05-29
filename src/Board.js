bdfb7e1de3f56737e89bc309b943da9a8858d0da
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-05-29 20:57:38 -0300

    fix: line count

--- src/Board.js
+++ src/Board.js
@@ -20,14 +20,14 @@ export default class Board extends React.Component {
           {this.renderSquare(2, 2, 0)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3, 0, 0)}
-          {this.renderSquare(4, 1, 0)}
-          {this.renderSquare(5, 2, 0)}
+          {this.renderSquare(3, 0, 1)}
+          {this.renderSquare(4, 1, 1)}
+          {this.renderSquare(5, 2, 1)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6, 0, 0)}
-          {this.renderSquare(7, 1, 0)}
-          {this.renderSquare(8, 2, 0)}
+          {this.renderSquare(6, 0, 2)}
+          {this.renderSquare(7, 1, 2)}
+          {this.renderSquare(8, 2, 2)}
         </div>
       </div>
     );
